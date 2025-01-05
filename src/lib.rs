14448d33057c89f3bc9491c9fea74a84903c8ff0
fibreyarn <uzivatel919@seznam.cz>
2025-01-05 23:08:07 +0100

    readme test

--- src/lib.rs
+++ src/lib.rs
@@ -986,6 +986,86 @@ mod tests_of_units {
             }
         }
     }
+
+    mod readme {
+        use crate::{Treerder, Orderable};
+
+        #[test]
+        fn test() {
+            let mut test = [
+                String::from("zzz"),
+                String::from("ZZZ"),
+                String::from("aaa"),
+                String::from("AAA"),
+            ];
+
+            let mut proof = test.clone();
+            proof.sort();
+
+            let mut orderer = Treerder::new();
+            orderer.order(&mut test);
+
+            assert_eq!(proof, test);
+        }
+
+        #[test]
+        fn test2() {
+            #[derive(Debug, PartialEq)]
+            struct LocalUsize(usize);
+
+            struct LocalUsizeCharIterator {
+                s: String,
+                c: usize,
+            }
+
+            impl Iterator for LocalUsizeCharIterator {
+                type Item = char;
+
+                fn next(&mut self) -> Option<char> {
+                    let c = self.c;
+
+                    let opt = self.s.chars().nth(c);
+
+                    if opt.is_some() {
+                        self.c = c + 1;
+                    }
+
+                    opt
+                }
+            }
+
+            impl Orderable for LocalUsize {
+                type Shadow = usize;
+
+                fn chars(&self) -> impl Iterator<Item = char> {
+                    LocalUsizeCharIterator {
+                        s: self.0.to_string(),
+                        c: 0,
+                    }
+                }
+
+                fn shadow(&self) -> Self::Shadow {
+                    self.0
+                }
+
+                fn steady(s: Self::Shadow) -> Self {
+                    LocalUsize(s)
+                }
+            }
+
+            fn ix(c: char) -> usize {
+                c.to_digit(10).unwrap() as usize
+            }
+
+            let mut nums = [999, 333, 33, 3, 0, 100, 10, 1].map(|x| LocalUsize(x));
+
+            let mut orderer = Treerder::<LocalUsize>::new_with(ix, 10);
+            orderer.order(&mut nums);
+
+            let proof = [0, 1, 10, 100, 3, 33, 333, 999].map(|x| LocalUsize(x));
+            assert_eq!(proof, nums);
+        }
+    }
 }
 
 // cargo test --features test-ext --release
